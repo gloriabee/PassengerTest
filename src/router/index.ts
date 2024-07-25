52ea681c44acbc2e6b70e23b30fe396f6c908f83
figmabebe <nawgloriawinnyunt_n@cmu.ac.th>
2024-07-26 01:08:54 +0700

    provide page not found for invalid url

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import PassengerDetailView from '@/views/passenger/DetailView.vue'
 import AirlineDetailView from '@/views/passenger/AirlineDetailView.vue'
 import LayoutView from '@/views/passenger/LayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +36,11 @@ const router = createRouter({
           props: true
         }
       ]
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     }
   ]
 })
